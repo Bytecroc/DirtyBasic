2a8d67fd7492817131166791f6afd73bf1049904
Include <antome@web.de>
2020-08-09 11:25:36 +0200

    Add files via upload

---
+++ dirtyIncludes/StdInclude.h
@@ -0,0 +1,98 @@
+
+#include <fstream>
+#include <iostream>
+#include <ios>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <locale>
+#include <algorithm>
+#include <windows.h>
+#include <cfloat>
+#include <cmath>
+#include <ctime>
+#include <cstddef>
+#include <cstdlib>
+#include <cstdarg>
+#include <cstdio>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <list>
+#include <memory>
+#include <new>
+#include <sys/time.h>
+
+//Fall conio.h nicht vorhanden, die nächsten zwei Zeilen löschen.
+#include <conio.h>
+#define waitkey getch()
+
+#define WriteTextFile ofstream
+#define ReadTextFile ifstream
+typedef  signed char      sbyte;
+//typedef  signed char      byte
+typedef  signed char      schar;
+typedef  unsigned char    uchar;
+typedef  unsigned char    ubyte;
+typedef  unsigned short   ushort;
+typedef  unsigned short   uword;
+typedef  signed short     sshort;
+typedef  signed short     sword;
+typedef  unsigned long    uint;
+typedef  signed long      sint;
+typedef  signed long      slong;
+typedef  unsigned long    ulong;
+typedef  const char * ctxt;
+typedef  char * fCstring;
+typedef  char * fcstring;
+typedef  double real;
+typedef  signed char      SBYTE;
+//typedef  signed char      BYTE
+typedef  signed char      SCHAR;
+typedef  unsigned char    UCHAR;
+typedef  unsigned char    UBYTE;
+typedef  unsigned short   USHORT;
+typedef  unsigned short   UWORD;
+typedef  signed short     SSHORT;
+typedef  signed short     SWORD;
+//typedef  unsigned long    UINT
+typedef  signed long      SINT;
+typedef  signed long      SLONG;
+typedef  unsigned long    ULONG;
+typedef  const char * CTXT;
+typedef  char * FCSTRING;
+typedef  double REAL;
+typedef  signed char      sByte;
+typedef  signed char      SByte;
+typedef  signed char      Byte;
+typedef  signed char      sChar;
+typedef  signed char      SChar;
+typedef  unsigned char    uChar;
+typedef  unsigned char    UChar;
+typedef  unsigned char    uByte;
+typedef  unsigned char    UByte;
+typedef  unsigned short   uShort;
+typedef  unsigned short   UShort;
+typedef  unsigned short   uWord;
+typedef  unsigned short   UWord;
+typedef  signed short     sShort;
+typedef  signed short     SShort;
+typedef  signed short     sWord;
+typedef  signed short     SWord;
+typedef  unsigned long    uInt;
+typedef  unsigned long    UInt;
+typedef  signed long      sInt;
+typedef  signed long      SInt;
+typedef  signed long      sLong;
+typedef  signed long      SLong;
+typedef  unsigned long    uLong;
+typedef  unsigned long    ULong;
+typedef  const char * cTxt;
+typedef  const char * CTxt;
+typedef  const char * Ctxt;
+typedef  char * FCstring;
+typedef  char * FCString;
+typedef  char * fCString;
+typedef  char * fcString;
+typedef  double Real;
+using namespace std;
